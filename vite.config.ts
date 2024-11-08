82ae771c72981c942abd0c3d9e7245ed6c9bb125
Vincent Wirawan <vincentnw24@gmail.com>
2024-11-08 13:42:39 -0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const { terser } = require('rollup-plugin-terser');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +8,10 @@ export default defineConfig({
     build: {
         minify: 'terser',
         rollupOptions: {
+            plugins: [terser()],
             output: {
                 manualChunks: {
                     vendor: ['react', 'react-dom'],
-                    // Add other large dependencies here
                 }
             }
         }
